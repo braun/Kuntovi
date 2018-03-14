7f744182c6c673d8928c48429202843a662bc93b
Stanislav Kunt <skunt@pointx.cz>
2018-03-14 14:37:23 +0100

    add pouch db datasource

--- db/setup.sh
+++ db/setup.sh
@@ -1,20 +1,19 @@
 export HOST=$1
-export PORT=$2
-export ROOT=$3
-export PWD=$4
+export ROOT=$2
+export PWD=$3
 export JSON_PWD=\"$PWD\"
-export SHOST="http://$ROOT:$PWD@$HOST:$PORT"
+export SHOST="http://$ROOT:$PWD@$HOST"
 echo $JSON_PWD
-curl -X PUT $HOST:$PORT/articles
-curl -X PUT $HOST:$PORT/images
+curl -X PUT $HOST/articles
+curl -X PUT $HOST/images
 
-curl -X PUT $HOST:$PORT/_config/admins/$ROOT -d $JSON_PWD
+curl -X PUT $HOST/_config/admins/$ROOT -d $JSON_PWD
 
 curl -X PUT $SHOST/_users/org.couchdb.user:braunie \
      -H "Accept: application/json" \
      -H "Content-Type: application/json" \
      -d '{"name": "braunie", "password": "vrkumr74", "roles": [ "blogger"], "type": "user"}'
 
-curl -X POST $SHOST/articles \
+xcurl -X POST $SHOST/articles \
 -H 'content-type:application/json' \
 -d $'{"_id":"_design/only_correct_user","validate_doc_update":"function (newDoc, oldDoc, userCtx) {console.error(userCtx.name);\\nif (userCtx.roles.indexOf(\'_admin\') == -1 && newDoc.user != userCtx.name) {\\nconsole.error(newDoc.user);throw({forbidden : \\"doc.user must be the same as your username.\\"});\\n}\\n}"}'
