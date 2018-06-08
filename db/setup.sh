84eafce37b0a3ac5578995c98b36d339bc0bfcc8
Stanislav Kunt <skunt@pointx.cz>
2018-06-08 20:58:57 +0200

    setup update

--- db/setup.sh
+++ db/setup.sh
@@ -1,11 +1,15 @@
 export HOST=$1
 export ROOT=$2
 export PWD=$3
+export PREFIX=$4
 export JSON_PWD=\"$PWD\"
 export SHOST="http://$ROOT:$PWD@$HOST"
+export ARTICLES_DB=$HOST/${PREFIX}_articles
+export IMAGES_DB=$HOST/${PREFIX}_images
+
 echo $JSON_PWD
-curl -X PUT $HOST/articles
-curl -X PUT $HOST/images
+curl -X PUT $ARTICLES_DB
+curl -X PUT $HOST/$IMAGES_DB
 
 curl -X PUT $HOST/_config/admins/$ROOT -d $JSON_PWD
 
@@ -14,6 +18,11 @@ curl -X PUT $SHOST/_users/org.couchdb.user:braunie \
      -H "Content-Type: application/json" \
      -d '{"name": "braunie", "password": "vrkumr74", "roles": [ "blogger"], "type": "user"}'
 
-curl -X POST $SHOST/articles \
+curl -X POST $ARTICLES_DB \
+-H 'content-type:application/json' \
+-d $'{"_id":"_design/only_correct_user","validate_doc_update":"function (newDoc, oldDoc, userCtx) {console.error(userCtx.name);\\nif (userCtx.roles.indexOf(\'_admin\') == -1 && newDoc.user != userCtx.name) {\\nconsole.error(newDoc.user);throw({forbidden : \\"doc.user must be the same as your username.\\"});\\n}\\n}"}'
+
+
+curl -X POST $IMAGES_DB \
 -H 'content-type:application/json' \
 -d $'{"_id":"_design/only_correct_user","validate_doc_update":"function (newDoc, oldDoc, userCtx) {console.error(userCtx.name);\\nif (userCtx.roles.indexOf(\'_admin\') == -1 && newDoc.user != userCtx.name) {\\nconsole.error(newDoc.user);throw({forbidden : \\"doc.user must be the same as your username.\\"});\\n}\\n}"}'
